1163fd80f4972dae189d2346282227b3937ff0fd
Jason <jasonetienne1@gmail.com>
2024-11-02 15:25:24 +0000

    adding functions

---
+++ cypress/page-objects/sections/bank-account-page-acc-section.ts
@@ -0,0 +1,21 @@
+import bankAccPageSelectors from "../selectors/bankAccPageSelectors";
+
+class bankAccountContainer {
+  verifyOnBankAccountPage() {
+    cy.get(bankAccPageSelectors.bankAccountContainer).should("be.visible");
+  }
+
+  checkIfAnyAccountsArePresent() {
+    return cy.get(bankAccPageSelectors.bankAccounts).then(($elements) => {
+      if ($elements.length > 0) {
+        cy.log(`Found $($elements.length) Bank Accounts`);
+        return $elements.length;
+      } else {
+        cy.log("No Bank Accounts Present");
+        return 0;
+      }
+    });
+  }
+}
+
+export default bankAccountContainer;
