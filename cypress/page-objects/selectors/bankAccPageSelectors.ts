79e89ebdd448f8f1ccb67af1ed40020d91179119
Jason <jasonetienne1@gmail.com>
2024-11-02 15:16:04 +0000

    adding selectors and sections file for bank acc POM

---
+++ cypress/page-objects/selectors/bankAccPageSelectors.ts
@@ -0,0 +1,8 @@
+const bankAccPageSelectors = {
+  bankAccounts: '[data-test="bankaccount-list"]',
+  createBankAccount: '[data-test="bankaccount-new"]',
+  deleteBankAccount: '[data-test="bankaccount-delete"]',
+  bankAccountContainer: ".MuiContainer-root",
+};
+
+export default bankAccPageSelectors;
--- cypress/page-objects/selectors/userProfileSidePanelSelectors.ts
+++ cypress/page-objects/selectors/userProfileSidePanelSelectors.ts
@@ -1,6 +1,7 @@
 const userProfileSideNavSelectors = {
   userProfilesSideNav: "[data-test='sidenav']",
   myAccount: "[data-test='sidenav-user-settings']",
+  bankAccounts: '[data-test="sidenav-bankaccounts"]',
 };
 
 export default userProfileSideNavSelectors;
