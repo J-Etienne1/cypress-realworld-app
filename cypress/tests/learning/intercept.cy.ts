4b0f249af0a04643ceaf4669e5098a2547c840f1
Jason <jasonetienne1@gmail.com>
2024-11-27 21:09:15 +0000

    stubbing API response

--- cypress/tests/learning/intercept.cy.ts
+++ cypress/tests/learning/intercept.cy.ts
@@ -1,9 +1,21 @@
 // https://dummyapi.io/explorer
 
-// <reference type="cypress"/>
-
 describe("Learning API Intercept", () => {
-  it("Simple API Intercept", () => {
+  it.skip("Simple API Intercept", () => {
+    cy.visit("https://dummyapi.io/explorer");
+
+    cy.intercept({
+      url: "https://dummyapi.io/data/v1/post/60d21af267d0d8992e610b8d/comment?limit=10",
+    }).as("comments");
+    cy.get(".flex > :nth-child(5)").click();
+    cy.wait("@comments").then((intercept) => {
+      expect(intercept.response?.body?.limit).eq(10);
+    });
+  });
+
+
+  // Stubbing
+  it("Mock API Response", () => {
     cy.visit("https://dummyapi.io/explorer");
 
     cy.intercept({
