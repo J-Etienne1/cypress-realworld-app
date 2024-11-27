159851a74cca8a6b2d5df851b0295e3e780bed98
Jason <jasonetienne1@gmail.com>
2024-11-27 21:26:35 +0000

    Mock API working

--- cypress/tests/learning/intercept.cy.ts
+++ cypress/tests/learning/intercept.cy.ts
@@ -1,7 +1,7 @@
 // https://dummyapi.io/explorer
 
 describe("Learning API Intercept", () => {
-  it.skip("Simple API Intercept", () => {
+  it("Simple API Intercept", () => {
     cy.visit("https://dummyapi.io/explorer");
 
     cy.intercept({
@@ -13,17 +13,23 @@ describe("Learning API Intercept", () => {
     });
   });
 
-
   // Stubbing
   it("Mock API Response", () => {
     cy.visit("https://dummyapi.io/explorer");
 
-    cy.intercept({
-      url: "https://dummyapi.io/data/v1/post/60d21af267d0d8992e610b8d/comment?limit=10",
-    }).as("comments");
+    cy.intercept(
+      "GET",
+      "https://dummyapi.io/data/v1/post/60d21af267d0d8992e610b8d/comment?limit=10",
+      {
+        limit: 10,
+        Name: "Testing Name",
+      }
+    ).as("comments");
     cy.get(".flex > :nth-child(5)").click();
     cy.wait("@comments").then((intercept) => {
+      console.log(intercept);
       expect(intercept.response?.body?.limit).eq(10);
+      expect(intercept.response?.body?.Name).eq("Testing Name");
     });
   });
 });
