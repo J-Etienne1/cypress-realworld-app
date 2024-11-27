d887681c20cc78ff6ea3e707e51d3d9398c83208
Jason <jasonetienne1@gmail.com>
2024-11-27 20:57:14 +0000

    can load site

---
+++ cypress/tests/learning/intercept.cy.ts
@@ -0,0 +1,9 @@
+// https://dummyapi.io/explorer
+
+// <reference type="cypress"/>
+
+describe("Learning API Intercept", () => {
+  it("Simple API Intercept", () => {
+    cy.visit("https://dummyapi.io/explorer");
+  });
+});
