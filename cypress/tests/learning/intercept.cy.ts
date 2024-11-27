60e7825fee1f40bec7a5803cb1717fbf299de417
Jason <jasonetienne1@gmail.com>
2024-11-27 21:06:57 +0000

    intercept worked

--- cypress/tests/learning/intercept.cy.ts
+++ cypress/tests/learning/intercept.cy.ts
@@ -5,5 +5,13 @@
 describe("Learning API Intercept", () => {
   it("Simple API Intercept", () => {
     cy.visit("https://dummyapi.io/explorer");
+
+    cy.intercept({
+      url: "https://dummyapi.io/data/v1/post/60d21af267d0d8992e610b8d/comment?limit=10",
+    }).as("comments");
+    cy.get(".flex > :nth-child(5)").click();
+    cy.wait("@comments").then((intercept) => {
+      expect(intercept.response?.body?.limit).eq(10);
+    });
   });
 });
